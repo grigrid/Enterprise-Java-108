e63c63ad7004efff8c3c27e7346eff4e1466ba86
Ingrid <ingbg80star@gmail.com>
2022-09-22 16:13:42 +0200

    ej2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@ public class Main {
         BigDecimal a2 = new BigDecimal("0.1578").setScale(2, RoundingMode.CEILING);
         BigDecimal result1 = a1.divide(a2);
        // round(a2);
-       System.out.println(round(result1));
+      // System.out.println(round(result1));
+       System.out.println(reverseSign(a1));
+
 
         //System.out.println(a1);
         //System.out.println(a2);
@@ -19,4 +21,9 @@ public class Main {
 
         return x.setScale(2, RoundingMode.CEILING).doubleValue();
     }
+    private static double reverseSign (BigDecimal x) {
+
+        return x.setScale(1, RoundingMode.CEILING).negate().doubleValue();
+    }
+
 }
